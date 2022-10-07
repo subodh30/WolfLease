9f0be8e5797f57608e2dfb89d035526c41c1829e
ameyagv <ameyav1234@gmail.com>
2022-10-06 22:44:06 -0400

    add delete test case for owner

--- housing/tests.py
+++ housing/tests.py
@@ -37,4 +37,13 @@ class OwnerTests(APITestCase):
         self.assertEqual(Owner.objects.count(), 1)
         self.assertEqual(Owner.objects.get().contact_email, 'test1@testing.com')
 
+    def test_delete_owner(self):
+                url = '/owners/'
+                data = {'contact_number': '1234567890', 'contact_email': 'test@testing.com', 'password': 'test'}
+                response = self.client.post(url, data, format='json')
+                url = url + str(Owner.objects.get().id) + '/'
+                data = {'contact_number': '1234567890', 'contact_email': 'test1@testing.com', 'password': 'test'}
+                response = self.client.delete(url, data, format='json')
+                self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+                self.assertEqual(Owner.objects.count(), 0)
     
